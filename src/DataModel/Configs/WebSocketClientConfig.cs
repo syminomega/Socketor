caab96fa801e605520305300cfcea5043d57aa42
Symin <syminomega@outlook.com>
2025-08-06 09:56:43 +0800

    修正WebSocketClient页面的样式

--- src/DataModel/Configs/WebSocketClientConfig.cs
+++ src/DataModel/Configs/WebSocketClientConfig.cs
@@ -3,7 +3,7 @@ namespace Socketor.DataModel.Configs;
 public class WebSocketClientConfig : IConnectionConfig
 {
     public string ConnectionType { get; set; } = nameof(WebSocketClientConfig);
-    public string WebSocketAddress { get; set; } = "ws://localhost:5000/chat";
+    public string WebSocketAddress { get; set; } = "ws://127.0.0.1:8080";
     public MessageBoxConfig MessageBoxConfig { get; set; } = new();
     public SendBoxConfig SendBoxConfig { get; set; } = new();
 
