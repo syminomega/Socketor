88f50345f8d1dbc0b49253e28d9b3dfea58fcdb1
Symin <syminomega@outlook.com>
2025-08-08 15:27:26 +0800

    移除在windows上不兼容的测试项

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,29 +1,18 @@
-use socketor_tcp_server::{TcpServerManager};
 use websocket_server::{WebSocketServerManager};
 use tokio::sync::Mutex;
 use tauri::Manager;
 
-mod socketor_tcp_server;
 mod websocket_server;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            app.manage(Mutex::new(TcpServerManager::default()));
             app.manage(Mutex::new(WebSocketServerManager::default()));
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet, 
-            socketor_tcp_server::start_tcp_server,
             websocket_server::start_websocket_server,
             websocket_server::stop_websocket_server,
             websocket_server::send_websocket_message,
